a00252b0921b2a57cdb1bc244c5c39cf2eb7f442
Naman Pahariya <namanpahariya21@gmail.com>
2025-11-14 19:07:10 +0530

    resolve in page-action file

--- components/ai/page-actions.tsx
+++ components/ai/page-actions.tsx
@@ -43,7 +43,8 @@ export function LLMCopyButton({
 
             return content;
           }).catch((error)=>{
-            console.log(error)
+            console.log(error);
+            return '';
           }),
         }),
       ]);
